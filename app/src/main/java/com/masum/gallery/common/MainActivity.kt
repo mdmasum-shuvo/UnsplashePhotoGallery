59e966f6daf7ebb37f255911c0e3f897b4403bc1
Masum Talukder <mdmasum.shuvo.cse@gmail.com>
2022-06-17 19:48:06 +0600

    build: add necessary dependency for the project

---
+++ app/src/main/java/com/masum/gallery/common/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.masum.gallery.common
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.masum.gallery.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
